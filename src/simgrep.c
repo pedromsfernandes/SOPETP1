b9e8aefed41a1fdad35ad5405f99876d54987de7
Rekicho <up201604145@fe.up.pt>
2018-03-15 19:20:18 +0000

    Splitting command arguments

--- src/simgrep.c
+++ src/simgrep.c
@@ -1,12 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
 #define MAX_OPTIONS 6
+#define MAX_PATTERN_SIZE 101
 
 int main(int argc, char* argv[])
 {
     if(argc < 2)
         return -1;
-        
-    char options[3][MAX_OPTIONS];
 
+    char options[MAX_OPTIONS][3];
+    char pattern[MAX_PATTERN_SIZE];
+
+    int i = 1;
+    int j = 0;
+
+    for(; i < argc; i++)
+    {
+        if(argv[i][0] == '-')
+            strcpy(options[j++],argv[i]);
+
+        else
+        {
+            strcpy(pattern,argv[i]);
+            break;
+        }
+    }
+
+    printf("%s\n",pattern);
 
     return 0;
 }
