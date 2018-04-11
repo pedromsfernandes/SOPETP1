3fcbad011a6f08de2b9ef216f23e874dd53a2f0c
Francisco Filipe <francilipe.7@gmail.com>
2018-04-11 10:55:17 +0100

    Installed ctrl + c signal handler

--- src/simgrep.cpp
+++ src/simgrep.cpp
@@ -10,8 +10,31 @@
 #include "findPattern.h"
 #include <dirent.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 using namespace std;
+
+void sigint_handler(int sig)
+{
+    sleep(10);
+    string answer = "";
+    // TODO: mandar sinal pausar
+
+    while (!(answer == "Y" || answer == "y" || answer == "N" || answer == "n"))
+    {
+        cout << "Are you sure you want to terminate the program ? (Y / N) ";
+
+        getline(cin, answer);
+        if (answer == "Y" || answer == "y")
+        {
+            //TODO: matar os filhos
+        }
+        else if (answer == "N" || answer == "n")
+        {
+            //TODO: mandar sinal continuar
+        }
+    }
+}
 
 int invalidArgs()
 {
@@ -88,7 +111,8 @@ void sweepDir(string pattern, string dirName, string options, bool isRec)
         closedir(d);
     }
 
-    for (unsigned int j = 0; j < filesInDir.size(); j++)
+    unsigned int size = filesInDir.size();
+    for (unsigned int j = 0; j < size; j++)
     {
         findPatternInFile(pattern, filesInDir[j], options, true);
     }
@@ -115,6 +139,12 @@ int main(int argc, char *argv[])
 
     int i = 1;
     int j = 0;
+
+    struct sigaction action;
+    action.sa_handler = sigint_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    sigaction(SIGINT, &action, NULL);
 
     for (; i < argc; i++)
     {
