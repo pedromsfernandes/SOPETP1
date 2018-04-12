88f7dd8683a1c4b410118fbf442082dcc17c6ac9
Francisco Filipe <francilipe.7@gmail.com>
2018-04-12 15:53:36 +0100

    Escrever aqui
    Agora escreves

--- src/simgrep.cpp
+++ src/simgrep.cpp
@@ -15,26 +15,32 @@
 
 using namespace std;
 
+int processGroup;
+
 void sigint_handler(int sig)
 {
-    sleep(10);
+    kill(-processGroup, SIGUSR1);
     string answer = "";
-    // TODO: mandar sinal pausar
 
     while (!(answer == "Y" || answer == "y" || answer == "N" || answer == "n"))
     {
-        cout << "Are you sure you want to terminate the program ? (Y / N) ";
+        cout << "\nAre you sure you want to terminate the program ? (Y / N) ";
 
         getline(cin, answer);
         if (answer == "Y" || answer == "y")
-        {
-            //TODO: matar os filhos
-        }
+            kill(-processGroup, SIGTERM);
         else if (answer == "N" || answer == "n")
-        {
-            //TODO: mandar sinal continuar
-        }
+            kill(-processGroup, SIGUSR2);
     }
+}
+
+void sigusr1_handler(int sig)
+{
+    pause();
+}
+
+void nothing(int sig)
+{
 }
 
 int invalidArgs()
@@ -145,7 +151,29 @@ int main(int argc, char *argv[])
     action.sa_handler = sigint_handler;
     sigemptyset(&action.sa_mask);
     action.sa_flags = 0;
-    sigaction(SIGINT, &action, NULL);
+    if (sigaction(SIGINT, &action, NULL) < 0)
+    {
+        cerr << "Couldn't install SIGINT handler\n";
+        exit(1);
+    }
+
+    action.sa_handler = nothing;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    if (sigaction(SIGUSR1, &action, NULL) < 0)
+    {
+        cerr << "Couldn't install SIGUSR1 handler\n";
+        exit(1);
+    }
+
+    action.sa_handler = nothing;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    if (sigaction(SIGUSR2, &action, NULL) < 0)
+    {
+        cerr << "Couldn't install SIGUSR2 handler\n";
+        exit(1);
+    }
 
     for (; i < argc; i++)
     {
@@ -201,17 +229,28 @@ int main(int argc, char *argv[])
     }
     else if (hasdir)
     {
+        processGroup = getpgrp();
         int pid = fork();
         int status = 0;
 
         if (pid == 0)
         {
+            action.sa_handler = sigusr1_handler;
+            sigemptyset(&action.sa_mask);
+            action.sa_flags = 0;
+            if (sigaction(SIGUSR1, &action, NULL) < 0)
+            {
+                cerr << "Couldn't install SIGUSR1 handler\n";
+                exit(1);
+            }
+
             sweepDir(pattern, filedir, options, recursive);
             exit(0);
         }
         else
         {
             wait(&status);
+            exit(status);
         }
     }
     else
